284750391d44e333c38dcfab03397385aa756827
Ankitjakhar123 <ANKITJAKHAR836@GMAIL.COM>
2025-06-07 11:32:21 +0530

    Update RadioStation.jsx

--- SoundLink Frontend/src/components/RadioStation.jsx
+++ SoundLink Frontend/src/components/RadioStation.jsx
@@ -28,8 +28,7 @@ const RadioStation = () => {
 
   const fetchStations = async () => {
     try {
-      setLoading(true);
-      const response = await fetch('https://de1.api.radio-browser.info/json/stations/bycountry/India');
+      setLoading(const response = await fetch('https://corsproxy.io/?https://de1.api.radio-browser.info/json/stations/bycountry/India'); 
       if (!response.ok) {
         throw new Error('Failed to fetch radio stations');
       }
